61bd57a27ef8c4c40a33030559eefb3700918ab2
Ubuntu <ubuntu@ip-10-0-0-110.ec2.internal>
2022-09-30 22:17:59 +0000

    possibly one forward pass iteration

--- src/tests/test_model_load.py
+++ src/tests/test_model_load.py
@@ -30,11 +30,22 @@ data_df = create_dataframe(combined_list)
 1. Test VisualBERT
 '''
 # Assumption: *get_visual_embeddings(image)* gets the visual embeddings of the image in the batch.
-from transformers import BertTokenizer, VisualBertForQuestionAnswering
+from transformers import BertTokenizer, VisualBertForQuestionAnswering, VisualBertForMultipleChoice
+from transformers import VisualBertModel, VisualBertConfig
 import torch
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
-model = VisualBertForQuestionAnswering.from_pretrained("uclanlp/visualbert-vqa")
+# model = VisualBertForMultipleChoice.from_pretrained("uclanlp/visualbert-vcr")
+# model = VisualBertForQuestionAnswering.from_pretrained("uclanlp/visualbert-vqa")
+
+# configuration = VisualBertConfig.from_pretrained("uclanlp/visualbert-vqa")
+configuration = VisualBertConfig.from_pretrained("uclanlp/visualbert-vcr")
+configuration.__dict__['visual_embedding_dim'] = 512
+print(configuration)
+model = VisualBertForMultipleChoice(configuration)
+
+configuration = model.config
+print(configuration)
 
 test_case_index = 88
 
@@ -44,12 +55,14 @@ visual_embeds = get_multiple_embeddings([data_df["image_path"][test_case_index],
 # print(next(iter(visual_embeds)))
 # list(test_dict.keys())[0]
 visual_embeds = visual_embeds[str(next(iter(visual_embeds)))]
-print(visual_embeds, len(visual_embeds), type(visual_embeds))
-visual_embeds = torch.Tensor(visual_embeds).unsqueeze(0)
+# print(visual_embeds, len(visual_embeds), type(visual_embeds))
+visual_embeds = torch.Tensor(visual_embeds).unsqueeze(0).unsqueeze(0)
 print(visual_embeds.shape)
 # visual_embeds = get_visual_embeddings(image).unsqueeze(0)
 visual_token_type_ids = torch.ones(visual_embeds.shape[:-1], dtype=torch.long)
 visual_attention_mask = torch.ones(visual_embeds.shape[:-1], dtype=torch.float)
+
+
 
 inputs.update(
     {
@@ -59,33 +72,88 @@ inputs.update(
     }
 )
 
-labels = torch.tensor([[0.0, 1.0]]).unsqueeze(0)  # Batch size 1, Num labels 2
-print(labels.shape)
+print(inputs.input_ids.shape)
+print(inputs.token_type_ids.shape)
+print(inputs.attention_mask.shape)
+print(inputs.visual_embeds.shape)
+print(inputs.visual_token_type_ids.shape)
+print(inputs.visual_attention_mask.shape)
 
-# outputs = model(**inputs, labels=labels)
+# labels = torch.tensor([[0.0, 1.0, 2.0, 3.0]]).unsqueeze(0)  # Batch size 1, Num labels 2
+# print(labels.shape)
+
+# outputs = model(**inputs)
 # loss = outputs.loss
 # scores = outputs.logits
+# print(scores)
 
-inputs = bert_tokenizer(
-        test_question,
-        padding="max_length",
-        max_length=20,
-        truncation=True,
-        return_token_type_ids=True,
-        return_attention_mask=True,
-        add_special_tokens=True,
-        return_tensors="pt",
-    )
 
-output_vqa = visualbert_vqa(
-    input_ids=inputs.input_ids,
-    attention_mask=inputs.attention_mask,
-    visual_embeds=features,
-    visual_attention_mask=torch.ones(features.shape[:-1]),
-    token_type_ids=inputs.token_type_ids,
-    output_attentions=False,
+print(text)
+prompt = data_df["question"][test_case_index]
+answer_choices = data_df["list_of_answers"][test_case_index]
+choice0 = answer_choices[0]
+choice1 = answer_choices[1]
+choice2 = answer_choices[2]
+choice3 = answer_choices[3]
+
+answer_ind = answer_choices.index(data_df['answer'][test_case_index])
+print(answer_ind)
+
+
+# # (batch_size, num_choices, visual_seq_length, visual_embedding_dim)
+visual_embeds = get_multiple_embeddings([data_df["image_path"][test_case_index],])
+visual_embeds = visual_embeds[str(next(iter(visual_embeds)))]
+visual_embeds = torch.Tensor(visual_embeds).unsqueeze(0)
+print(visual_embeds.shape)
+visual_embeds = visual_embeds.expand(1, len(answer_choices), *visual_embeds.shape)
+print(visual_embeds.shape)
+visual_token_type_ids = torch.ones(visual_embeds.shape[:-1], dtype=torch.long)
+visual_attention_mask = torch.ones(visual_embeds.shape[:-1], dtype=torch.float)
+
+labels = torch.tensor(answer_ind).unsqueeze(0)  # choice0 is correct (according to Wikipedia ;)), batch size 1
+print(labels)
+
+prompts = [[prompt]*len(answer_choices)]
+prompts = sum(prompts, [])
+print(prompts)
+
+choices = [[choice0, choice1, choice2, choice3]]
+choices = sum(choices, [])
+print(choices)
+
+encoding = tokenizer(prompts, choices, return_tensors="pt", padding=True)
+print(encoding['input_ids'])
+print(encoding['input_ids'].shape)
+
+# # batch size is 1
+inputs_dict = {k: v.unsqueeze(0) for k, v in encoding.items()}
+# inputs_dict = {k: [v[i:i+4] for i in range(0, len(v), 4)] for k, v in encoding.items()}
+
+print(inputs_dict['input_ids'][0][0])
+print(inputs_dict['input_ids'][0][1])
+print(inputs_dict['input_ids'][0][2])
+print(inputs_dict['input_ids'][0][3])
+print(tokenizer.decode(inputs_dict['input_ids'][0][0]))
+print(tokenizer.decode(inputs_dict['input_ids'][0][1]))
+print(tokenizer.decode(inputs_dict['input_ids'][0][2]))
+print(tokenizer.decode(inputs_dict['input_ids'][0][3]))
+print(inputs_dict['input_ids'])
+print(inputs_dict['input_ids'][0].shape)
+
+# Check sizes of all of these inputs!!
+
+inputs_dict.update(
+{
+        "visual_embeds": visual_embeds,
+        "visual_attention_mask": visual_attention_mask,
+        "visual_token_type_ids": visual_token_type_ids,
+        "labels": labels,
+    }
 )
-# get prediction
-pred_vqa = output_vqa["logits"].argmax(-1)
-print("Question:", test_question)
-print("prediction from VisualBert VQA:", vqa_answers[pred_vqa])
+
+print(inputs_dict)
+outputs = model(**inputs_dict)
+loss = outputs.loss
+logits = outputs.logits
+print(logits.argmax(-1))
+print(answer_choices[logits.argmax(-1)])
