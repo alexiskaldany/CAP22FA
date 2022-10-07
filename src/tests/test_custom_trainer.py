17da341080d2e1dbb913aec17b853b0a0d830b19
Ubuntu <ubuntu@ip-10-0-0-110.ec2.internal>
2022-10-07 21:16:45 +0000

    finishing training loop

--- src/tests/test_custom_trainer.py
+++ src/tests/test_custom_trainer.py
@@ -18,6 +18,7 @@ from datetime import datetime
 import datetime as dt
 import random
 import pandas as pd
+from loguru import logger
 
 class VQAModel:
     '''
@@ -38,12 +39,9 @@ class VQAModel:
         self.valid_data_loader = valid_data_loader
         self.test_data_loader = test_data_loader
 
-        self.LOG_FILENAME = log_file
-        if self.LOG_FILENAME:
-            '''
-            Does logging here
-            '''
-            pass
+        self.LOGGING = log_file
+        if self.LOGGING:
+            logger.info(f"Setting up model VisualBERT")
 	
     def train(self, num_epochs=1, lr=5e-5, sample_every=100, eval_during_training=False, save_weights=True, model_weights_dir='./results/model_weights/') :
         '''
@@ -64,22 +62,21 @@ class VQAModel:
 
         num_epochs = num_epochs
         num_training_steps = num_epochs * len(self.train_data_loader)
-        # num_training_steps = num_epochs * 1
         lr_scheduler = get_scheduler(name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps)
 
         self.model.resize_token_embeddings(len(self.tokenizer))
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
         print('Using device..', device)
-        # if LOG_FILENAME:
-        #     MY_LOGGER.info(f"{datetime.now()} -- [Model Training] Using device {device}...")	
+        if self.LOGGING:
+            logger.info(f"{datetime.now()} -- [Model Training] Using device {device}...")	
 
         self.model.to(device)
 
         progress_bar = tqdm(range(num_training_steps))
 
         total_t0 = time.time()
-        training_stats = []
+        self.training_stats = []
         sample_every = sample_every
 
         gc.collect()
@@ -89,9 +86,9 @@ class VQAModel:
             print("")
             print('======== Epoch {:} / {:} ========'.format(epoch + 1, num_epochs))
             print(f'Training {self.model._get_name()}...')
-            # if LOG_FILENAME:
-            #     MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training] \n======== Epoch {epoch + 1} / {num_epochs} ========")	
-            #     MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training] Training... {model_type}")	
+            if self.LOGGING:
+                logger.info(f"{datetime.now()} -- [Model Training] \n======== Epoch {epoch + 1} / {num_epochs} ========")	
+                logger.info(f"{datetime.now()} -- [Model Training] Training... {self.model_type}")	
             
             total_train_loss = 0
             total_train_accuracy = 0
@@ -111,35 +108,39 @@ class VQAModel:
                 b_visual_attention_mask = batch['visual_attention_mask'][0].to(device)
                 b_visual_token_type_ids = batch['visual_token_type_ids'][0].to(device)
 
-                # print(b_input_ids.shape)
-                # print(b_token_type_ids.shape)
-                # print(b_attention_mask.shape)
-                # print(b_labels.shape)
-                # print(b_visual_embeds.shape)
-                # print(b_visual_attention_mask.shape)
-                # print(b_visual_token_type_ids.shape)
+                # logger.info(f"Shapes of - b_input_ids: {b_input_ids.shape}, \
+                #                 b_token_type_ids: {b_token_type_ids.shape}, \
+                #                 b_attention_mask: {b_attention_mask.shape}, \
+                #                 b_labels: {b_labels.shape}, \
+                #                 b_visual_embeds: {b_visual_embeds.shape}, \
+                #                 b_visual_attention_mask: {b_visual_attention_mask.shape}, \
+                #                 b_visual_token_type_ids: {b_visual_token_type_ids.shape}, \
+                #                 ")
 
                 self.model.zero_grad()        
 
                 outputs = self.model(input_ids=b_input_ids, attention_mask=b_attention_mask, token_type_ids=b_token_type_ids, visual_embeds=b_visual_embeds, visual_attention_mask=b_visual_attention_mask, visual_token_type_ids=b_visual_token_type_ids, labels=b_labels)
 
                 loss = outputs[0]
-
                 batch_loss = loss.item()
-                # batch_perplexity = math.exp(batch_loss)
                 
-                total_train_loss += batch_loss
+                logits = outputs[1]
+                y_pred = logits.argmax(-1)
+                train_acc = torch.sum(y_pred == b_labels)
 
-                print('LOSS: ', loss, batch_loss, total_train_loss)
-                # total_train_perplexity += batch_perplexity
+                total_train_loss += batch_loss
+                logger.info(f'LOSS:  {loss}, {batch_loss}, {total_train_loss}')
+                logger.info(f'Predicted: {y_pred}, Target: {b_labels}, Accuracy: {train_acc}')
+
+                total_train_accuracy += train_acc
                     
-                # # Get sample every x batches.
+                # # Get sample every x batches to evaluate.
                 # if step % sample_every == 0 and not step == 0:
 
                 #     elapsed = format_time(time.time() - t0)
                 #     print('  Batch {:>5,}  of  {:>5,}. Loss: {:>5,}.   Elapsed: {:}.'.format(step, len(train_data_loader), batch_loss, elapsed))
-                #     if LOG_FILENAME:
-                #         MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training]   Batch {step}  of  {len(train_data_loader)}. Loss: {batch_loss}.   Elapsed: {elapsed}.")	
+                #     if self.LOGGING:
+                #         logger.info(f"{datetime.now()} -- [Model Training]   Batch {step}  of  {len(train_data_loader)}. Loss: {batch_loss}.   Elapsed: {elapsed}.")	
 
                 #     if eval_during_training:
                 #         model.eval()
@@ -156,8 +157,8 @@ class VQAModel:
                 #                             )
                 #         for i, sample_output in enumerate(sample_outputs):
                 #             print("{}: {}".format(i, tokenizer.decode(sample_output, skip_special_tokens=True)))
-                #             if LOG_FILENAME:
-                #                 MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training] {i}: {tokenizer.decode(sample_output, skip_special_tokens=True)}")	
+                #             if self.LOGGING:
+                #                 logger.info(f"{datetime.now()} -- [Model Training] {i}: {tokenizer.decode(sample_output, skip_special_tokens=True)}")	
                         
                         # model.train()
 
@@ -167,20 +168,20 @@ class VQAModel:
                 progress_bar.update(1)
 
             # Calculate the average loss over all of the batches.
-            avg_train_loss = total_train_loss / len(self.train_data_loader)       
-            # avg_train_perplexity = total_train_perplexity / len(train_data_loader)       
+            avg_train_loss = total_train_loss / len(self.train_data_loader)
+            avg_train_accuracy = total_train_accuracy/len(self.train_data_loader)       
 
             # # Measure how long this epoch took.
             training_time = self.format_time(time.time() - t0)
 
-            print("")
-            print("  Average training loss: {0:.2f}".format(avg_train_loss))
-            # print("  Average training perplexity: {0:.2f}".format(avg_train_perplexity))
-            print("  Training epoch took: {:}".format(training_time))
-            # if LOG_FILENAME:
-            #     MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training]\n  Average training loss: {avg_train_loss}")	
-            #     MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training]\n  Average training perplexity: {avg_train_perplexity}")	
-            #     MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training]  Training epoch took: {training_time}")
+            # print("")
+            # print("  Average training loss: {0:.2f}".format(avg_train_loss))
+            # print("  Average training accuracy: {0:.2f}".format(avg_train_accuracy))
+            # print("  Training epoch took: {:}".format(training_time))
+            if self.LOGGING:
+                logger.info(f"{datetime.now()} -- [Model Training]\n  Average training loss: {avg_train_loss}")	
+                logger.info(f"{datetime.now()} -- [Model Training]\n  Average training accuracy: {avg_train_accuracy}")	
+                logger.info(f"{datetime.now()} -- [Model Training]  Training epoch took: {training_time}")
 
             # ========================================
 			#               Validation
@@ -188,71 +189,78 @@ class VQAModel:
 
             print("")
             print("Running Validation...")
-            # if self.LOG_FILENAME:
-            #     self.MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Validation] Running Validation...")	
+            if self.LOGGING:
+                logger.info(f"{datetime.now()} -- [Model Validation] Running Validation...")	
 
-        #     t0 = time.time()
+            t0 = time.time()
 
-        #     self.model.eval()
+            self.model.eval()
 
-        #     total_eval_loss = 0
-        #     total_eval_perplexity = 0
-        #     nb_eval_steps = 0
+            total_eval_loss = 0
+            total_eval_accuracy = 0
+            nb_eval_steps = 0
 
-        #     # Evaluate data for one epoch
-        #     for batch in self.valid_data_loader:
+            # Evaluate data for one epoch
+            for batch in self.valid_data_loader:
                 
-        #         b_input_ids = batch[0].to(device)
-        #         b_labels = batch[0].to(device)
-        #         b_masks = batch[1].to(device)
+                b_input_ids = batch['input_ids'][0].to(device)
+                b_token_type_ids = batch['token_type_ids'][0].to(device)
+                b_attention_mask = batch['attention_mask'][0].to(device)
 
-        #         with torch.no_grad():        
+                b_labels = batch['labels'][0].to(device)
 
-        #             outputs  = self.model(b_input_ids, 
-        # #                            token_type_ids=None, 
-        #                             attention_mask = b_masks,
-        #                             labels=b_labels)
+                b_visual_embeds = batch['visual_embeds'][0].to(device)
+                b_visual_attention_mask = batch['visual_attention_mask'][0].to(device)
+                b_visual_token_type_ids = batch['visual_token_type_ids'][0].to(device)
+
+                with torch.no_grad():        
+
+                    outputs = self.model(input_ids=b_input_ids, attention_mask=b_attention_mask, token_type_ids=b_token_type_ids, visual_embeds=b_visual_embeds, visual_attention_mask=b_visual_attention_mask, visual_token_type_ids=b_visual_token_type_ids, labels=b_labels)
                 
-        #             loss = outputs[0]
-                    
-        #         batch_loss = loss.item()
-        #         batch_perplexity = math.exp(batch_loss)
+                    loss = outputs[0]
+                    logits = outputs[1]
+                
+                batch_loss = loss.item()
 
-        #         total_eval_loss += batch_loss
-        #         total_eval_perplexity += batch_perplexity
+                y_pred = logits.argmax(-1)
+                val_acc = torch.sum(y_pred == b_labels)
 
+                total_eval_loss += batch_loss
+                total_eval_accuracy += val_acc
+                logger.info(f'[Model Validation] - LOSS: {loss}, {batch_loss}, {total_eval_loss}')
+                logger.info(f'[Model Validation] - Predicted: {y_pred}, Target: {b_labels}, Accuracy: {val_acc}')
 
-        #     avg_val_loss = total_eval_loss / len(self.valid_data_loader)
-        #     avg_val_perplexity = total_eval_perplexity / len(self.valid_data_loader)
+            avg_val_loss = total_eval_loss / len(self.valid_data_loader)
+            avg_val_accuracy = total_eval_accuracy / len(self.valid_data_loader)
             
-        #     validation_time = self.format_time(time.time() - t0)    
+            validation_time = self.format_time(time.time() - t0)    
 
         #     print("  Validation Loss: {0:.2f}".format(avg_val_loss))
         #     print("  Validation Perplexity: {0:.2f}".format(avg_val_perplexity))
         #     print("  Validation took: {:}".format(validation_time))
-        #     if self.LOG_FILENAME:
-        #         self.MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Validation]   Validation Loss: {avg_val_loss}")	
-        #         self.MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Validation]   Validation Perplexity: {avg_val_perplexity}")	
-        #         self.MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Validation]     Validation took: {validation_time}")	
+            if self.LOGGING:
+                logger.info(f"{datetime.now()} -- [Model Validation]   Validation Loss: {avg_val_loss}")	
+                logger.info(f"{datetime.now()} -- [Model Validation]   Validation Accuracy: {avg_val_accuracy}")	
+                logger.info(f"{datetime.now()} -- [Model Validation]     Validation took: {validation_time}")	
 
-        #     # Record all statistics from this epoch.
-        #     self.training_stats.append(
-        #         {
-        #             'epoch': epoch + 1,
-        #             'Training Loss': avg_train_loss,
-        #             'Training Perplexity': avg_train_perplexity,
-        #             'Valid. Loss': avg_val_loss,
-        #             'Valid. Perplexity': avg_val_perplexity,
-        #             'Training Time': training_time,
-        #             'Validation Time': validation_time
-        #         }
-        #     )
+            # Record all statistics from this epoch.
+            self.training_stats.append(
+                {
+                    'epoch': epoch + 1,
+                    'Training Loss': avg_train_loss,
+                    'Training Accuracy': avg_train_accuracy,
+                    'Valid. Loss': avg_val_loss,
+                    'Valid. Accuracy': avg_val_accuracy,
+                    'Training Time': training_time,
+                    'Validation Time': validation_time
+                }
+            )
 
         # print("")
         # print("Training complete!")
         # print("Total training took {:} (h:mm:ss)".format(self.format_time(time.time()-total_t0)))
-        # if self.LOG_FILENAME:
-        #     self.MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel Training] Training complete!\n Total training took {self.format_time(time.time()-total_t0)} (h:mm:ss)")
+        if self.LOGGING:
+            logger.info(f"{datetime.now()} -- [Model Training] Training complete!\n Total training took {self.format_time(time.time()-total_t0)} (h:mm:ss)")
 
         if save_weights:
             self.model_weights_dir = model_weights_dir
@@ -268,8 +276,8 @@ class VQAModel:
         if not os.path.exists(self.model_weights_dir):
             os.makedirs(self.model_weights_dir)
 
-        # if self.LOG_FILENAME:
-        #     self.MY_LOGGER.info(f"{datetime.now()} -- [LanguageModel] Saving model to {self.model_weights_dir}...")
+        if self.LOGGING:
+            logger.info(f"{datetime.now()} -- [Model] Saving model to {self.model_weights_dir}...")
         print("Saving model to %s" % self.model_weights_dir)
 
         # Save a trained model, configuration and tokenizer using save_pretrained()`.
@@ -295,12 +303,12 @@ class VQAModel:
 
     def get_training_stats(self, save_weights=True, model_weights_dir='./results/model_weights/training_stats.csv'):
         '''
-        Method to get trainig stats
+        Method to get training stats
         
         Params:
             self: instance of object	
         Returns:
-            df_stats (pandas df): trainig stats	
+            df_stats (pandas df): training stats	
         '''
         # Create a DataFrame from our training statistics.
         df_stats = pd.DataFrame(data=self.training_stats)
@@ -313,6 +321,7 @@ class VQAModel:
 
         # Display the table.
         print(df_stats.head(100))
+        logger.info(f'{df_stats.head(100)}')
 
         return df_stats
 
